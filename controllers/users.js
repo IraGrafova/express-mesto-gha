1f987f36a626c6961f82ca381b3e910e25eed081
IraGrafova <i.grafova@ntc-hitech.ru>
2023-06-17 21:23:12 +0300

    refactor validation createUser

--- controllers/users.js
+++ controllers/users.js
@@ -38,11 +38,13 @@ const createUser = ('/users', (req, res) => {
 });
 
 const changeUser = ('/users/me', (req, res) => {
-  User.findByIdAndUpdate(req.user._id, req.body)
+  User.findByIdAndUpdate(req.user._id, req.body, {
+    new: true, // обработчик then получит на вход обновлённую запись
+    runValidators: true})
     .orFail(() => new Error('Not found'))
     .then((user) => res.status(200).send(user))
     .catch((err) => {
-      if (err.message.includes('validation failed')) {
+      if (err.message.includes('Validation failed')) {
         res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
       } else if (err.message === 'Not found') {
         res.status(404).send({
@@ -55,11 +57,14 @@ const changeUser = ('/users/me', (req, res) => {
 });
 
 const changeUserAvatar = ('/users/me/avatar', (req, res) => {
-  User.findByIdAndUpdate(req.user._id, req.body)
+  User.findByIdAndUpdate(req.user._id, req.body, {
+    new: true, // обработчик then получит на вход обновлённую запись
+    runValidators: true,
+})
     .orFail(() => new Error('Not found'))
     .then((user) => res.status(200).send(user))
     .catch((err) => {
-      if (err.message.includes('validation failed')) {
+      if (err.message.includes('Validation failed')) {
         res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' });
       } else if (err.message === 'Not found') {
         res.status(404).send({

c4ecd144510a8491a6b1a3a2d73f6c9eadc699fc
IraGrafova <i.grafova@ntc-hitech.ru>
2023-06-28 22:34:26 +0300

    do joi

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ const {
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
     avatar: Joi.string().required(),
